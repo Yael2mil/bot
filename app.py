27a3e74b38f2c2532b56121d56abe4f5607f4b4c
Yael2mil <1719110459@utectulancingo.edu.mx>
2021-03-22 06:12:38 +0000

    funciona

---
+++ app.py
@@ -0,0 +1,45 @@
+import tensorflow.keras
+from PIL import Image, ImageOps
+import numpy as np
+
+
+def importarImagen(ruta):
+    # Disable scientific notation for clarity
+    np.set_printoptions(suppress=True)
+    # Load the model
+    model = tensorflow.keras.models.load_model('keras_model.h5')
+    # Create the array of the right shape to feed into the keras model
+    # The 'length' or number of images you can put into the array is
+    # determined by the first position in the shape tuple, in this case 1.
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    
+    image = Image.open(ruta)
+
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+    image_array = np.asarray(image)
+
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+    data[0] = normalized_image_array
+
+    prediction = model.predict(data)
+
+    for i in prediction:
+        if i[0] > 0.8:
+            resultado = "Mamifero"
+            return resultado
+        elif i[1] > 0.8:
+            resultado = "Ave"
+            return resultado
+        elif i[2] > 0.8:
+            resultado = "Reptil"
+            return resultado
+        elif i[3] > 0.8:
+            resultado = "Pez"
+            return resultado
+        else:
+            resultado = "no se logro identificar una especie para esta imagen"
+            return resultado
+
---
+++ bot.py
@@ -0,0 +1,83 @@
+import sys
+import os 
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from app import importarImagen
+
+
+token = "1755655581:AAG29qRQS1SBK6yPsQ3D6hL0zGdYDg74BnU"
+
+def start(bot, update):
+    try:
+        username = update.message.from_user.username
+        message = "Hola " + username
+        update.message.reply_text(message) 
+    except Exception as error:
+        print ("Error 001 {}".format(error.args[0]))
+
+
+def echo(bot, update):
+    try:
+        text = update.message.text
+        update.message.reply_text(text)
+    except Exception as error:
+        print ("Error 002 {}".format(error.args[0]))
+
+
+def help(bot, update):
+    try:
+        message = "Puedes enviar texto o imagenes"
+        update.message.reply_text(message)
+    except Exception as error:
+        print ("Error 003 {}".format(error.args[0]))
+
+def error(bot, update, error):
+    try:
+        print (error)
+    except Exception as e:
+        print ("Error 004 {}".format(e.args[0]))
+
+def main():
+    try:
+        updater = Updater(token)
+        dp = updater.dispatcher
+
+        dp.add_handler(CommandHandler("start", start))
+        dp.add_handler(CommandHandler("help", help))
+
+        dp.add_handler(MessageHandler(Filters.text,echo))
+        dp.add_handler(MessageHandler(Filters.photo, getImage))
+
+        dp.add_error_handler(error)
+
+        updater.start_polling()
+        updater.idle()
+        print('bot listo')
+
+    except Exception as e:
+        print ("Error 005 {}".format(e.args[0]))
+
+def getImage(bot, update):
+    try:
+        message = 'Recibiendo imagen'
+        update.message.reply_text(message)
+
+        file = bot.getFile(update.message.photo[-1].file_id)
+        id = file.file_id
+        
+        filename = os.path.join("descargas/", "{}.jpg".format(id))
+        file.download(filename)
+
+        
+        message = 'Imagen guardada'
+        update.message.reply_text(message)
+        ruta= 'descargas/{}.jpg'.format(id)
+        message = importarImagen(ruta)
+        update.message.reply_text(message)
+    except Exception as e:
+        print("Error 007 {}".format(e))
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as error:
+        print ("Error 006  {}".format(error))
